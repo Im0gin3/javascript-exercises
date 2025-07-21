c295a40cf941676f13c650f374937a604c2ffa76
Im0gin3 <113782157+Im0gin3@users.noreply.github.com113782157+Im0gin3@users.noreply.github.com>
2025-07-22 00:07:09 +0530

    01 to 05

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -8,7 +8,14 @@
 */
 
 // Add your code right below, good luck!
-
+firstName="Carlos";
+lastName="Stevenson";
+fullName=firstName+" "+lastName
+thisYear=1965;
+birthYear=1947
+age=thisYear-birthYear
+let greeting=("Hello! My name is "+fullName+" and I am "+age+" years old.")
+console.log(greeting)
 
 
 
--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(text,num) {
+    if (num>0){
+        num--;
+        return text;
+        
+    }
 };
+repeatString("hey",3);
 
 // Do not edit below this line
 module.exports = repeatString;
