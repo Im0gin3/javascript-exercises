7997536dc7681aaea0d57cf312f4816979cf9897
Im0gin3 <113782157+Im0gin3@users.noreply.github.com113782157+Im0gin3@users.noreply.github.com>
2025-08-03 10:41:04 +0530

    fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let num1=1;
+    let num2=1
+    if (typeof num === "string"){
+        num = +num;
+    }
+    if (num===num1){
+        return num1;
+    } else if (num===2){
+        return num2;
+    } else if (num===0){
+        return 0;
+    } else if (num<0){
+        return "OOPS";
+    }
+    for (let i = 2; i<num; i++){
+        result=num1+num2
+        num1=num2;
+        num2=result;
+    }
+    return num2;
 };
 
 // Do not edit below this line
