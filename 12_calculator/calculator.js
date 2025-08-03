24c948ea7c2566904cf332e0435d60ac2b86b328
Im0gin3 <113782157+Im0gin3@users.noreply.github.com113782157+Im0gin3@users.noreply.github.com>
2025-08-03 09:36:23 +0530

    make calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+};
+const multiply = function(arr) {
+  return arr.reduce((accumulator,currentValue)=> accumulator*currentValue,1);
 };
 
-const multiply = function() {
-
+const power = function(a,b) {
+  let result = 1;
+	for (let i =0;i<b;i++){
+    result *= a;
+  }
+  return result;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+	let num=a;
+  let result = 1;
+  if (num===0){
+    return result;
+  }
+  while (num>1){
+    result *= num;
+    num--; 
+  }
+  return result;
 };
 
 // Do not edit below this line
