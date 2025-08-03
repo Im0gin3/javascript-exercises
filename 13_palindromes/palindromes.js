6e33e59aff77755f9cc210b141afde6a7d59db43
Im0gin3 <113782157+Im0gin3@users.noreply.github.com113782157+Im0gin3@users.noreply.github.com>
2025-08-03 10:02:04 +0530

    Palindrome done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const cleaned = word
+    .toLowerCase()
+    .split("")
+    .filter((item)=>item !== "," && item !== "." && item !== " " && item !== "!" && item !== " " && item !== "?")
+    .join("")
+    const compare = cleaned
+    .split("")
+    .reverse()
+    .join("")
 
+    if (compare === cleaned){
+        return true;
+    } else{
+        return false;
+    }
 };
 
 // Do not edit below this line
