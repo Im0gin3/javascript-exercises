09546dd3e280280c938c3c53197de3818ffadcfc
Im0gin3 <113782157+Im0gin3@users.noreply.github.com113782157+Im0gin3@users.noreply.github.com>
2025-08-08 15:47:29 +0530

    done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let oldIndex=0;
+    let oldVal, toCompare;
+    const arrLen=arr.length;
+    const currentYear=new Date().getFullYear();
+    for (let i = 0; i<arrLen;i++){
+        if (arr[i].yearOfDeath===undefined){
+            arr[i].yearOfDeath=currentYear;
+        }
+    }
+    oldVal=arr[0].yearOfDeath - arr[0].yearOfBirth;
+    for (let i = 1; i<arrLen;i++){
+        toCompare = arr[i].yearOfDeath - arr[i].yearOfBirth;
+        if (toCompare>oldVal){
+            oldIndex=i;
+            oldVal = toCompare;
+        }
+    }
+    return arr[oldIndex];
 };
+// make a variable called oldest which can store the index. 
+// loop through the array through for loop nd just store the older one. 
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
